f445b6099daaaf53d817719405380f1a15ed19ca
Sohel Ali Mandal <sohel.mandal@siemens.com>
2020-02-25 10:29:31 +0530

    test

---
+++ FirstAppVS/FirstAppVS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+
+{
+    class Program
+    {
+        //static void Main (strings[] args)
+        //static int Main (strings[] args)
+        //static int Main ()
+        // possible signature
+        static void Main()
+        {
+
+            // string filepath = @"d:\";
+            Console.WriteLine("hello");
+            Console.WriteLine("checking the code snippets");
+            //Console.wr
+        }
+    }
+}
