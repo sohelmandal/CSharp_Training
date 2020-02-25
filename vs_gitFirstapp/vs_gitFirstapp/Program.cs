029874f1642a533d560f872eae1c7ee44603d210
Sohel Ali Mandal <sohel.mandal@siemens.com>
2020-02-25 11:35:51 +0530

    added library

---
+++ vs_gitFirstapp/vs_gitFirstapp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vs_gitFirstapp
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            int result = Add(12, 13);
+            Console.WriteLine(result);
+            Console.WriteLine("press any key to end..");
+            Console.ReadLine();    
+        }
+        static int Add(int firstValue, int secondvalue)
+        {
+            return (firstValue + secondvalue);
+
+        }
+
+    }
+}
